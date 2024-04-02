a50622556414139b36632dbffdcc616d0b3a9aab
SubeLucas <subelucas.pro@gmail.com>
2024-04-02 21:34:37 +0200

    structure du projet via maven

---
+++ projet/src/main/java/Joubert_Mineau_Sauzeau_Sube/App.java
@@ -0,0 +1,13 @@
+package Joubert_Mineau_Sauzeau_Sube;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
