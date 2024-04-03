ab1e38474d9b03ad4643339fb12f2a2516b2885f
AntoineSauzeau <72159603+AntoineSauzeau@users.noreply.github.com>
2024-04-03 09:08:41 +0200

    Implémentation du constructeur de Dataframe qui prend un chemin vers un ficheir csv

--- projet/src/main/java/Joubert_Mineau_Sauzeau_Sube/Dataframe.java
+++ projet/src/main/java/Joubert_Mineau_Sauzeau_Sube/Dataframe.java
@@ -1,33 +1,43 @@
 package main.java.Joubert_Mineau_Sauzeau_Sube;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Vector;
+import javax.xml.crypto.Data;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class Dataframe {
 
     HashMap<String, Vector<Object>> matElements;
 
     public Dataframe(String[] columnNames, Object[]... columnArrays){
-        matElements = new HashMap<String, Vector<Object>>();
-
-        int i = 0;
-        String columnName = "";
-        for(Object[] columnArray : columnArrays){
-            Vector<Object> columnVector = new Vector<>(Arrays.asList(columnArray));
-            if (i >= columnNames.length) {
-                columnName = String.valueOf(i);
-            }
-            else{
-                columnName = columnNames[i];
-            }
-            matElements.put(columnName, columnVector);
-            i++;
-        }
+        createDataframe(columnNames, columnArrays);
     }
 
-    public Dataframe(String filename){
+    public Dataframe(String filename) throws IOException {
 
+        Vector<Vector<String>> records = new Vector<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("book.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(";");
+                records.add((Vector<String>) Arrays.asList(values));
+            }
+        }
+
+        String[] columnNames = new String[records.get(0).size()];
+        columnNames = (String[]) records.get(0).toArray();
+
+        Object[][] columns = new Object[records.size()-1][];
+        for(int l = 1; l < records.size(); l++){
+            Object[] column = new Object[records.size()];
+            for(int c = 0; c < records.get(l).size(); c++){
+                column[c] = records.get(l).get(c);
+            }
+            columns[l-1] = column;
+        }
+
+        createDataframe(columnNames, columns);
     }
 }
